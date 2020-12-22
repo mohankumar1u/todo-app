1e70f23bc3376b13ef4cf5f7af916fa14c0aaed5
mohankumar1u <mohankumar.uppu@gmail.com>
2020-12-22 13:48:25 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("working");
+const https = require('https');
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
+const cors =require('cors');
+const port =  process.env.PORT || 2000;
+//const sequelize = './db.js'
+app.use(cors());
+app.listen(port, () => console.log(`server started on port ${port}`));
+app.get('/data', (req, res) => {
+    return "working";
+  });
